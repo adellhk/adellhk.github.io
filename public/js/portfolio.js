3c75ad04119efa814d9aec0e1163439e6e9fd89e
Adell Hanson-Kahn <adell.hk@gmail.com>
2015-03-26 01:36:29 -0700

    adds tiles for eyecatchers, wtp app, rockshoe

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,17 +1,25 @@
+function bindInfoState(target, message) {
+	$(target).hover(function() {
+		tileInfoState($(target), message);
+	}, function() {
+		resetTile($(target));
+	});
+};
+
+function tileInfoState(tile, displayText) {
+	tile.html('<span class="display-text">'+displayText+'</span>');
+};
+
+function resetTile(tile) {
+	tile.html("");
+};
+
 $(document).ready(function($) {
 
-	$("#wtpTile").hover(function() {
-		tileInfoState($(this), "We The PAC - take your vote beyond the ballot box.");
-	}, function() {
-		resetTile($(this));
-	});
+	bindInfoState("#wtpTile", "We The PAC - take your vote beyond the ballot box.");
+	bindInfoState("#rockshoeTile", "Rockshoe.me - shop for rock climbing shoes with fitment reviews.");
+	bindInfoState("#wtpAppTile", "We The PAC Android App - browse wethepac.net content from your phone in realtime.");
+	bindInfoState("#eyecatchersTile", "Eyecatchers.me - responsively designed photography portfolio built on Bootstrap.");
 
-	function tileInfoState(tile, displayText) {
-		tile.html('<span class="display-text">'+displayText+'</span>');
-	};
-
-	function resetTile(tile) {
-		tile.html("");
-	};
 
 });
